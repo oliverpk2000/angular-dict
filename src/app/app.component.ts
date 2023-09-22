42e50ce2058c77957f3622c5bdc01962fcdad4b7
oliver <oliver.pecek@gmail.com>
2023-09-22 10:18:04 +0200

    general progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DictionaryService} from "./dictionary.service";
+import {Definition, Information} from "../objects/information";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Word} from "../objects/word";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-dict';
+
+  inf: Information[] = []
+
+  wordForm = new FormGroup({
+    value: new FormControl("example", [Validators.required])
+  })
+
+  constructor(public dictionary: DictionaryService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  search(word: string): void{
+    this.dictionary.getWordInformation(word).subscribe(res => {
+      res.forEach(r => this.inf.push(r))
+      console.log(res)
+      console.log(this.inf[0])
+    });
+  }
+
+
+  getWord() {
+    let word: Word =  this.wordForm.value as Word;
+
+  }
 }
--- src/app/dictionary.service.ts
+++ src/app/dictionary.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Definition, Information, Information} from "../objects/information";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,10 @@ export class DictionaryService {
 
   baseUrl: string = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-  constructor(httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
+  getWordInformation(word:string): Observable<Information[]>{
+    return this.httpClient.get<Information[]>(this.baseUrl + word);
+  }
 
 }
